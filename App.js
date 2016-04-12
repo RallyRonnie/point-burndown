018b95764060ce385155f616aa601437531722f5
William Kammersell <william.kammersell@ca.com>
2016-04-12 12:33:53 -0600

    Base example of PI Burnup from App SDK examples.

--- App.js
+++ App.js
@@ -1,9 +1,147 @@
-Ext.define('CustomApp', {
-    extend: 'Rally.app.App',
-    componentCls: 'app',
-    launch: function() {
-        //Write app code here
-
-        //API Docs: https://help.rallydev.com/apps/2.1/doc/
-    }
-});
+Ext.define('Rally.example.BurnCalculator', {
+	extend: 'Rally.data.lookback.calculator.TimeSeriesCalculator',
+		config: {
+			completedScheduleStateNames: ['Accepted']
+		},
+			
+		constructor: function(config) {
+			this.initConfig(config);
+			this.callParent(arguments);
+		},
+			
+		getDerivedFieldsOnInput: function() {
+			var completedScheduleStateNames = this.getCompletedScheduleStateNames();
+			return [
+				{
+					"as": "Planned",
+   					"f": function(snapshot) {
+						if (snapshot.PlanEstimate) {
+							return snapshot.PlanEstimate;
+						}
+						return 0;
+					}
+				},
+				{
+					"as": "PlannedCompleted",
+					"f": function(snapshot) {
+						if (_.contains(completedScheduleStateNames, snapshot.ScheduleState) && snapshot.PlanEstimate) {
+							return snapshot.PlanEstimate;
+						}
+						return 0;
+					}
+				}
+			];
+		},
+			
+		getMetrics: function() {
+			return [
+				{
+					"field": "Planned",
+					"as": "Planned",
+					"display": "line",
+					"f": "sum"
+				},
+				{
+					"field": "PlannedCompleted",
+					"as": "Completed",
+					"f": "sum",
+					"display": "column"
+				}
+			];
+		}
+	});
+			
+	var PI_OID = 123456789; //The ObjectID of the PI on which to burn
+			
+	Ext.define('CustomApp', {
+		extend: 'Rally.app.App',
+		requires: [
+			'Rally.example.BurnCalculator'
+		],
+			
+		launch: function() {
+			this.add({
+				xtype: 'rallychart',
+				storeType: 'Rally.data.lookback.SnapshotStore',
+				storeConfig: this._getStoreConfig(),
+				calculatorType: 'Rally.example.BurnCalculator',
+				calculatorConfig: {
+					completedScheduleStateNames: ['Accepted', 'Released']
+				},
+					chartConfig: this._getChartConfig()
+				});
+			},
+			
+			/**
+			 * Generate the store config to retrieve all snapshots for all leaf child stories of the specified PI
+			 */
+			_getStoreConfig: function() {
+				return {
+					find: {
+						_ItemHierarchy: PI_OID,
+						_TypeHierarchy: 'HierarchicalRequirement',
+						Children: null
+					},
+					fetch: ['ScheduleState', 'PlanEstimate'],
+					hydrate: ['ScheduleState'],
+					sort: {
+						_ValidFrom: 1
+					},
+					context: this.getContext().getDataContext(),
+					limit: Infinity
+				};
+			},
+			
+			/**
+			 * Generate a valid Highcharts configuration object to specify the chart
+			 */
+			_getChartConfig: function() {
+				return {
+					chart: {
+						defaultSeriesType: 'area',
+						zoomType: 'xy'
+					},
+					title: {
+						text: 'PI Burnup'
+					},
+					xAxis: {
+						categories: [],
+						tickmarkPlacement: 'on',
+						tickInterval: 5,
+						title: {
+							text: 'Date',
+							margin: 10
+						}
+					},
+					yAxis: [
+						{
+							title: {
+								text: 'Points'
+							}
+						}
+					],
+					tooltip: {
+						formatter: function() {
+							return '' + this.x + '<br />' + this.series.name + ': ' + this.y;
+						}
+					},
+					plotOptions: {
+						series: {
+							marker: {
+								enabled: false,
+								states: {
+									hover: {
+										enabled: true
+									}
+								}
+							},
+							groupPadding: 0.01
+						},
+						column: {
+							stacking: null,
+							shadow: false
+						}
+					}
+				};
+			}
+		});
